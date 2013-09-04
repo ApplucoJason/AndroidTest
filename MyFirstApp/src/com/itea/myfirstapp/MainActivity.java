5e83e644bdab6f5f08ef28cefc2f4e01ad60b593
Jason Tian <jason.tian@i-tea.com.tw>
2013-09-04 20:30:29 +0800

    Linear Layout Practise
    1. Linear Layout practise
    2. build a second.xml
    3. do nothing

--- MyFirstApp/src/com/itea/myfirstapp/MainActivity.java
+++ MyFirstApp/src/com/itea/myfirstapp/MainActivity.java
@@ -2,14 +2,17 @@ package com.itea.myfirstapp;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
+
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.second);
+        Log.v("MyFirstApp", "onCreate");
     }
 
 
@@ -17,7 +20,43 @@ public class MainActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+        
+        Log.v("MyFirstApp", "onCreateOptionsMenu");
         return true;
     }
+
+
+	@Override
+	protected void onRestart() {
+		// TODO Auto-generated method stub
+		Log.v("MyFirstApp", "onRestart");
+		super.onRestart();
+	}
+
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		Log.v("MyFirstApp", "onResume");
+		super.onResume();
+	}
+
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		Log.v("MyFirstApp", "onPause");
+		super.onPause();
+	}
+
+
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		Log.v("MyFirstApp", "onDestroy");
+		super.onDestroy();
+	}
+    
+    
     
 }
